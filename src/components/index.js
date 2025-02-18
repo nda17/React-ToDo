1006972be908c8050b7337adcb908c4bf0d8c104
Dmitry <khv1702@gmail.com>
2025-02-18 20:28:20 +0300

    Added reexport config

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import { EmptyPlaceholder } from './EmptyPlaceholder/EmptyPlaceholder';
+import { NewTaskForm } from './NewTaskForm/NewTaskForm';
+import { Task } from './Task/Task';
+import { TaskList } from './TaskList/TaskList';
+import { TasksFilter } from './TasksFilter/TasksFilter';
+
+export { EmptyPlaceholder, NewTaskForm, Task, TaskList, TasksFilter };
